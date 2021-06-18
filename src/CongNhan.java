14e714431068706ffbfa3a76a9c3c88e4c2237e2
zhuy-nguyenz <graio911@gmail.com>
2021-06-19 06:14:27 +0700

    1st Commit

--- src/CongNhan.java
+++ src/CongNhan.java
@@ -2,23 +2,10 @@ public class CongNhan extends CanBo {
     private String type;
     private String specialty;
 
-    public CongNhan() {
-    }
-
-    public CongNhan(String type, String specialty) {
-        this.type = type;
-        this.specialty = specialty;
-    }
-
     public CongNhan(String name, String dateOfBirth, String gender, String address, String type, String specialty) {
         super(name, dateOfBirth, gender, address);
         this.type = type;
         this.specialty = specialty;
-    }
-
-    public CongNhan(String name, String dateOfBirth, String gender, String address) {
-        super(name, dateOfBirth, gender, address);
-
     }
 
     public String getType() {
